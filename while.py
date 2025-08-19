ee09d7f2d92ecedb2744228f9f06a53902e9c36e
Abhi290703 <abhigec6@gmail.com>
2025-08-19 22:09:37 +0530

    commit all

---
+++ while.py
@@ -0,0 +1,37 @@
+# i = 5
+# while True :
+#     i+=3
+#     print (i)
+#     if i==20:
+#         break
+
+# i = 0
+# while i<10 :
+#     i+=1
+#     print (i)
+# else:
+#     print("no repetition")
+
+''' inreverse rightangle triangle '''
+# i=9
+# while i>0 :
+#     i-=1 # i=i-2
+#     print("*"*(i+1))
+
+# ''' in rightangle triangle '''
+# i=0
+# while i<9 :
+#     i+=1
+#     print(' '*(9-i)+'*'*i)
+
+''' reverse value '''
+for i in range(0,6,1):
+    print(5-i)
+''' Even and odd number'''
+for num in range(1,2):
+    if num%2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+''' '''
