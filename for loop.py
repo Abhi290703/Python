4db33d5753bfa9ac3d170cc961bb6aa8f39d7c93
Abhi290703 <abhigec6@gmail.com>
2025-08-20 14:44:05 +0530

    commit all

--- for loop.py
+++ for loop.py
@@ -1,13 +1,13 @@
-# ''' reverse value '''
-# for i in range(0,6,1):
-#     print(5-i)
+''' reverse value '''
+for i in range(0,6,1):
+    print(5-i)
 
-# ''' Even and odd number'''
-# for i in range(1,2):
-#     if i%2 == 0:
-#         print("even")
-#     else:
-#         print("odd")
+''' Even and odd number'''
+for i in range(1,2):
+    if i%2 == 0:
+        print("even")
+    else:
+        print("odd")
 
 ''' factorial '''
 fact = 4
@@ -15,14 +15,14 @@ for i in range(1,5):
     fact = fact * i
 print(fact)
 
-# ''' fibonacci series'''
-# a = 0
-# b = 1
-# for i in range(0,10):
-#     c = a + b
-#     a = b
-#     b = c
-#     print(a,end=' ')
+''' fibonacci series'''
+a = 0
+b = 1
+for i in range(0,10):
+    c = a + b
+    a = b
+    b = c
+    print(a,end=' ')
 
 
 ''' factorial '''
