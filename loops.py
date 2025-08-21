8750aeceb035a909b952fd20ce5bd02bb5227861
Abhi290703 <abhigec6@gmail.com>
2025-08-21 14:49:20 +0530

    commit all

--- loops.py
+++ loops.py
@@ -1,12 +1,67 @@
-time=int(input("Enter time in hours: "))
-time = float(input("Enter time in hours: "))
+time=int(input("enter time:"))
+time=float(input("enter time in float value"))
 if time < 6:
-    print("you can easily go to institution")
+    print("easy to go ")
 elif time < 7:
-    print("you can go but bit late to institution")
-elif time < 7.5:
-    print("you can take leave")
-elif time == 8:
-    print("your class is from online")
-elif time > 8:
-    print("you should not go to institution")
+    print("you can go bit late")
+elif time < 8:
+    print("you are so late now")
+else:
+    print("you go and sleep")
+
+''' if condition'''
+
+car = "BMW"
+if car == "volvo":
+    print(car,"that you owned")
+else:
+    print(car,"that yours is not owned")
+
+''' else if condition '''
+car = "hyundi"
+if car == "BMW":
+    print(car, "is a luxury car")
+elif car == "Benz":
+    print(car,"it is a premium car")
+elif car == "volvo":
+    print(car,"most safest car")
+elif car == "range rover":
+    print(car,"hulk of cars")
+else:
+    print(car,"is not a luxury car")
+
+''' while loop '''
+i = 8
+# while True:
+#     i -=1
+#     print(i)
+#     if i > 5:
+#         continue
+#     else:   
+#         break
+
+# ''' reverse'''
+# for i in range(0,11,1):
+#     print(2-i)
+
+''' factorial '''
+fact = 7
+for i in range(1,fact+1):
+    fact=fact*i
+print(fact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ string.py
@@ -0,0 +1,15 @@
+name = "Abhi"
+print (name[2])
+
+# length of string
+print(len(name))   
+
+# type of string
+print(type(name))
+
+# directory of string
+print(dir(name))
+
+# using for loop
+for i in range(0, len(name)):
+    print(name[i])
