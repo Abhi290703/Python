1df387108aa2e147b720adefcce75e15299ef2e2
Abhi290703 <abhigec6@gmail.com>
2025-08-21 15:34:20 +0530

    commit all

--- string.py
+++ string.py
@@ -13,3 +13,28 @@ print(dir(name))
 # using for loop
 for i in range(0, len(name)):
     print(name[i])
+
+# reversing the number
+rev=name[::-1]
+print(rev)
+
+# by using for loop
+rev = ''
+for i in range (len(name)-1,-1,-1):
+    rev+=name[i]
+if rev == name:
+    print("it is a palindrome")
+else:
+    print("it is not a palindrome")
+
+# to find letter in between
+
+print(name.find('b'))  # returns index of first occurrence
+print(name.find('d'))
+print(name.find('h',3,5))
+
+# rfinder 
+ 
+print(name.rfind('s'))  # returns index of last occurrence
+print(name.rfind('d'))
+print(name.rfind('h',2,6))
