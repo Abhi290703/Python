7a268c66889aca615ef92b479622466e60d89592
Abhi290703 <abhigec6@gmail.com>
2025-08-22 11:13:48 +0530

    add all

---
+++ pratice.py
@@ -0,0 +1,71 @@
+# tuple = ("apple","banana","mango")
+# print(tuple)
+
+# list = ["apple","banana","mango"]
+# print(list)
+
+# '''string reverse'''
+# a = "apple"
+# rev = a[::-1]
+# print(rev)
+
+# ''' if else statement '''
+# if a == rev:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# # ''' program 2 '''
+# marks = 75
+# age = 18
+# if marks >= 90:
+#     print("A")
+#     if age < 18:
+#         print("You are eligible for scholarship")
+#     elif (marks > 70 and marks < 80):
+#         print("B grade")
+#         if age >= 21:
+#             print("You are not eligible for scholarship")
+# else:
+#     print("fail")
+
+# ''' program 3 '''
+# login = "yes"
+# device = "desktop"
+# if login == "device":
+#     # if device == "mobile":
+#         print ("mobile dashboard")
+# else:
+#         print("desktop dashboard")
+
+# '''program 4'''
+# # type conversions
+
+# number = 6
+# print(bin(number))
+# print(hex(number))
+# print(oct(number))
+# # print(decimal(number))
+
+# # type conversions 
+
+# print(type(int("10")))  # string to int
+# print(int(10.5))  # float to int
+# print(int(78))
+# print(int(False))
+
+# print(float(24.89))
+# print(float(10))
+# print(float(False))
+# print(float(True))
+# print(type(float(23.67)))
+
+# print(bool(1))  # int to bool
+# print(bool(23))
+# print(type(bool(True)))
+
+# string concatenation
+first_name = 'John'
+last_name = 'srinu'
+full_name = first_name + " " + last_name
+print(full_name)
