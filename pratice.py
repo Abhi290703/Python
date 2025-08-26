6c8c2c80ad7ac546c6e35e6a40c7a1f4d7107d54
Abhi290703 <abhigec6@gmail.com>
2025-08-27 01:53:47 +0530

    add

---
+++ pratice.py
@@ -0,0 +1,26 @@
+# if-else statement program
+
+a = 20
+b = 0
+if (a < b):
+    print("largest number")
+else :
+    print("smallest number")
+
+# even and odd number
+
+c = 12
+num = 0
+if(num % 2 == 0):
+    print("even number")
+else:
+    print("odd number")
+
+# if statement
+
+d  = 10
+i = 10
+if(i == d):
+    print("my number")
+else :
+    print("your number")
