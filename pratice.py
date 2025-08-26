4377ba34263c7456cf4fd06fd3b5ef272b46586d
Abhi290703 <abhigec6@gmail.com>
2025-08-27 02:43:37 +0530

    commit

--- pratice.py
+++ pratice.py
@@ -1,43 +1,90 @@
 # if-else statement program
 
-a = 20
-b = 0
-if (a < b):
-    print("largest number")
-else :
-    print("smallest number")
+# a = 20
+# b = 0
+# if (a < b):
+#     print("largest number")
+# else :
+#     print("smallest number")
 
 # even and odd number
 
-c = 12
-num = 0
-if(num % 2 == 0):
+# c = 12
+# num = 0
+# if(num % 2 == 0):
+#     print("even number")
+# else:
+#     print("odd number")
+
+# if statement
+
+# d  = 10
+# e = 10
+# if(e == d):
+#     print("my number")
+# else :
+    # print("your number")
+
+# else if statement
+
+# marks = int(input("enter a number"))
+
+# if marks >=90:
+#     print("A+ grade")
+# elif marks >= 75 and marks <= 89:
+#         print("A grade")
+# elif marks >= 55 and marks <= 74:
+#         print("B grade")
+# elif marks >= 45 and marks <= 54:
+#         print("D grade")
+# else:
+#     print("Fail")
+
+# checking whether condition the given number is positive or negative
+
+# f = 0
+# if(f > 0):
+#     print("positive number")
+# elif f < 0:
+#     print("negative number")
+# else:
+#     print("zero")
+
+
+# age category
+
+# age = int(input("enter a age"))
+
+# if age < 13:
+#     print("child")
+# elif age >= 18 and age <= 20 :
+#     print("youth")
+# elif age >= 21 and age <= 29:
+#     print("teenage")
+# elif age >= 30 and age <= 59:
+#     print("gentleman")
+# else:
+#     if age > 60:
+#         print("senior citizen")
+#     else:
+#         print("not a senior citizen")
+
+# determine if the number is +,-,0
+
+num = 11
+if num == 0:
+    print("zero")
+elif num % 2 == 0:
     print("even number")
 else:
     print("odd number")
 
-# if statement
 
-d  = 10
-e = 10
-if(e == d):
-    print("my number")
-else :
-    print("your number")
 
-# else if statement
 
-marks = 70
 
-if ("marks = 70"):
-    if (marks >=90):
-        print("A+ grade")
-    elif marks >= 75 and marks <= 89:
-        print("A grade")
-    elif marks >= 55 and marks <= 74:
-        print("B grade")
-    elif marks >= 45 and marks <= 54:
-        print("D grade")
-else :
-    if (marks <= 44):
-        print("Fail")
+
+
+
+
+
