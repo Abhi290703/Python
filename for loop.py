ff2db1f361d3c0bf797f85d8fe63163e50363710
Abhi290703 <abhigec6@gmail.com>
2025-08-27 16:45:26 +0530

    commit

--- for loop.py
+++ for loop.py
@@ -19,24 +19,37 @@
 #     print(fact)
 
 # 1 to 100 prime numbers
-for i in range (2,103):
-    count = 0
-    for j in range (1,i+1):
-        if i%j == 0:
-            count+=1
-    if count == 2:
-        print(i,"it is a prime number")
-    else:
-        print(i,"it is not a prime number")
+# for i in range (2,103):
+#     count = 0
+#     for j in range (1,i+1):
+#         if i%j == 0:
+#             count+=1
+#     if count == 2:
+#         print(i,"it is a prime number")
+#     else:
+#         print(i,"it is not a prime number")
 
 
 # matrics
-for i in range (1,5):
-    for j in range(1,4):
-        print(i,'-',j,end = " ")
-    print(' ')
+# for i in range (1,5):
+#     for j in range(1,4):
+#         print(i,'-',j,end = " ")
+#     print(' ')
+
+# right angle triangle 
+for i in range(1,6):
+    for j in range(1,6):
+        if i<=j:
+            print('*', end =" ")
+    print(" ")
 
 
+# invert right angle triangle
+for i in range(1,5):
+    for j in range(1,5):
+        if i>=j:
+            print('*', end = " ")
+    print(" ")
 
 
 
