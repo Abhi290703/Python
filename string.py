f3525aea11e50af3fa8f759bac70dce061bfb378
Abhi290703 <abhigec6@gmail.com>
2025-08-29 10:36:27 +0530

    add

--- string.py
+++ string.py
@@ -1,40 +1,48 @@
-name = "Abhi"
-print (name[2])
+# name = "Abhi"
+# print (name[2])
 
-# length of string
-print(len(name))   
+# # length of string
+# print(len(name))   
 
-# type of string
-print(type(name))
+# # type of string
+# print(type(name))
 
-# directory of string
-print(dir(name))
+# # directory of string
+# print(dir(name))
 
-# using for loop
-for i in range(0, len(name)):
-    print(name[i])
+# # using for loop
+# for i in range(0, len(name)):
+#     print(name[i])
 
-# reversing the number
-rev=name[::-1]
-print(rev)
+# # reversing the number
+# rev=name[::-1]
+# print(rev)
 
-# by using for loop
-rev = ''
-for i in range (len(name)-2,-1,-1):
-    rev+=name[i]
-if rev == name:
-    print("it is a palindrome")
-else:
-    print("it is not a palindrome")
+# # by using for loop
+# rev = ''
+# for i in range (len(name)-2,-1,-1):
+#     rev+=name[i]
+# if rev == name:
+#     print("it is a palindrome")
+# else:
+#     print("it is not a palindrome")
 
-# to find letter in between
+# # to find letter in between
 
-print(name.find('b'))  # returns index of first occurrence
-print(name.find('d'))
-print(name.find('h',2,3))
+# print(name.find('b'))  # returns index of first occurrence
+# print(name.find('d'))
+# print(name.find('h',2,3))
 
-# rfinder 
+# # rfinder 
  
-print(name.rfind('s'))  # returns index of last occurrence
-print(name.rfind('d'))
-print(name.rfind('h',2,6))
+# print(name.rfind('s'))  # returns index of last occurrence
+# print(name.rfind('d'))
+# print(name.rfind('h',2,6))
+
+#  string methods
+
+name1 = "achiversIT"
+print(len(name1))
+print(name1.rfind('i'))
+# print(name1.rfind('i',0,10,2))
+print(name1.count('i',10))
