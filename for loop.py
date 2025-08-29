6b388f2bdc1347d74ce0dd0399ec16f0a784cadd
Abhi290703 <abhigec6@gmail.com>
2025-08-29 15:00:00 +0530

    for loop add

--- for loop.py
+++ for loop.py
@@ -12,11 +12,11 @@
 
 # factorial number
 
-# n = int(input("enter a factorial :"))
-# fact = 1
-# for i in range(1,n+1):
-#     fact = fact*i
-#     print(fact)
+n = int(input("enter a factorial :"))
+fact = 1
+for i in range(1,n+1):
+    fact*=i
+print(fact)
 
 # 1 to 100 prime numbers
 # for i in range (2,103):
@@ -68,42 +68,42 @@
 
 # print sum of numbers
 
-n = 20
-sum = 0
-for i in range(0,n+1):
-    sum+=i
-print(sum)
+# n = 20
+# sum = 0
+# for i in range(0,n+1):
+#     sum+=i
+# print(sum)
 
 # display numbers using loop
-num = [1,2,3,4,5,6,7,8]
-for i in num:
-    print(i)
+# num = [1,2,3,4,5,6,7,8]
+# for i in num:
+#     print(i)
 
 # count total number od digits
-num = 1,2,3,4,5,6,7,8,9,0
-count = 0
-for i in num:
-    count+=1
-print(count)
+# num = 1,2,3,4,5,6,7,8,9,0
+# count = 0
+# for i in num:
+#     count+=1
+# print(count)
 
 
 # given string is palindrome or not
-a = "madam  "
-rev_str = ""
-for i in a:
-    rev_str=i+rev_str
-if(a == rev_str):
-    print(a,"it is a palindrome")
-else:
-    print(a,"it is not a plaindrome")
+# a = "madam  "
+# rev_str = ""
+# for i in a:
+#     rev_str=i+rev_str
+# if(a == rev_str):
+#     print(a,"it is a palindrome")
+# else:
+#     print(a,"it is not a plaindrome")
 
 
 
 
 # user given string is reverse
 
-user = "string"
-rev_str = " "
-for i in user:
-    rev_str=i+rev_str
-print(rev_str)
+# user = "string"
+# rev_str = " "
+# for i in user:
+#     rev_str=i+rev_str
+# print(rev_str)
