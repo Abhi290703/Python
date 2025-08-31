757acaf31e237ec91ed0ba3b6c7affc958cd978f
Abhi290703 <abhigec6@gmail.com>
2025-08-31 10:34:00 +0530

    add

--- while.py
+++ while.py
@@ -1,37 +1,37 @@
-# i = 1
-# while True :
-#     i+=1
-#     print (i)
-#     if i<10:
-#         continue
-#     else:
-#         break
+# i = 1  
+while True :
+    i+=1
+    print (i)
+    if i<10:
+        continue
+    else:
+        break
 
 # i = 0
-# while i<10 :
-#     i+=1
-#     print (i)
-# else:
-#     print("no repetition")
+while i<10 :
+    i+=1
+    print (i)
+else:
+    print("no repetition")
 
 # ''' inreverse rightangle triangle '''
-# i=9
-# while i>0 :
-#     i-=1 # i=i-2
-#     print("*"*(i+1))    
+i=9
+while i>0 :
+    i-=1 # i=i-2
+    print("*"*(i+1))    
 
 # ''' in rightangle triangle '''
-# i=0
-# while i<9 :
-#     i+=1
-#     print(' '*(9-i)+'*'*i)
+i=0
+while i<9 :
+    i+=1
+    print(' '*(9-i)+'*'*i)
 
 # second method of rigth angle triangle
 
-# j = 0
-# while j < 7:
-#     print("*"*j)
-#     j+=1
+j = 0
+while j < 7:
+    print("*"*j)
+    j+=1
 
 
 # print("************************")
@@ -39,17 +39,13 @@
 
 # invert method
 
-# j = 6
-# while j > 1:
-#     print("*"*j)
-#     j-=1
+j = 6
+while j > 1:
+    print("*"*j)
+    j-=1
 
 # print 1 to 10 numbers
 
-i = 1
-while i <= 10:
-    print(i)
-    i+=1
 
 # reverse of 1 to 10 numbers
 
