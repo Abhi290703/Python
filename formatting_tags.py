17adbd1d0f097adfc6478feee9888d1088aec134
Abhi290703 <abhigec6@gmail.com>
2025-09-02 12:12:55 +0530

    add

--- formatting_tags.py
+++ formatting_tags.py
@@ -9,5 +9,15 @@ print(ord(value))
 memory = 32
 device = "hardware"
 price = 21.90
-print('it is a',memory,'gb',device,'coast about',price)
-print('it is a %dgb %s coast about %f'%(memory,device,price))
+# print('it is a',memory,'gb',device,'cost about',price)
+# print('it is a %dgb %s cost about $%f'%(memory,device,price))
+# print('it is a',device, memory, 'gb cost about', price )
+# print('it is a %d gb %s cost about $%f'%(device,price,memory))
+
+
+# space conviction
+print('it is a {}gb {} cost about ${}'. format(memory,device,price))
+print('it is a {}gb {} cost about ${}'. format(device,memory,price))
+print('it is a {1}gb {0} cost about ${2}'. format(device,memory,price))
+
+print(f'it is a {memory}gb {device} cost about ${price}')
