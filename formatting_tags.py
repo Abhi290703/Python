92b158b0f74004f1569fa63ff7644c84051a8f00
Abhi290703 <abhigec6@gmail.com>
2025-09-02 11:47:05 +0530

    add

--- formatting_tags.py
+++ formatting_tags.py
@@ -4,3 +4,10 @@ value = "A"
 print(ord(value))
 
 # c style printing
+# it is a 32gb hard drive coast about 21.90
+
+memory = 32
+device = "hardware"
+price = 21.90
+print('it is a',memory,'gb',device,'coast about',price)
+print('it is a %dgb %s coast about %f'%(memory,device,price))
