80d7e1b133fa456338a0a028a523685ffa09203a
Abhi290703 <abhigec6@gmail.com>
2025-09-09 11:03:35 +0530

    add dictonary

--- dictonary.py
+++ dictonary.py
@@ -30,9 +30,42 @@ for i in d3['age']:
 
 ''' we can modify the value i it '''
 
-d3['name'][0] ='puni'
-print(d3)
+# d3['name'][0] ='puni'
+# print(d3)
 
+# zip function
 
+l1 = [1,2,3,4]
+l2 = ['one','two','three']
+l3 = dict(zip(l1,l2))
+print(l3)
 
+''' if there is no dict keyword o/p will not be generated '''
+
+# undefined value
+
+l4 = [1,2,3,4,5]
+l5 = ['one','two','three','four']
+l6 = dict(zip(l4,l5))
+print(l6)
+
+''' has we seen here we have given undefined value where output was not generated for it was generated 
+upto defined value.
+'''
+
+# enumarted value
+
+l6 = dict(enumerate(l5))
+print(l6)
+
+l6 = dict(enumerate(l5,start=1000))
+print(l6)
+
+l5 = dict(enumerate(l5,start=20002201))
+print(l5)
+
+''' comprehension with iterable pairs'''
+
+l7 = {x:y for x,y in l4}
+print(l7)
 
