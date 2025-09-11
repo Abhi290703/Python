fd515c5af38193a7765d7d5070db392aee4cf566
Abhi290703 <abhigec6@gmail.com>
2025-09-11 13:54:25 +0530

    add dictonary

---
+++ dictonary/dict-methods.py
@@ -0,0 +1,27 @@
+''' 
+we are representing values
+1.keys
+2.values
+3.set
+4.get
+'''
+
+''' obtaining keys '''
+
+d1 = {1:"one",2:"two",3:"three",4:"four",5:"five"}
+a = d1.keys()
+print(a)
+
+for x in a:
+    print(d1[x])
+
+''' obtainig values '''
+
+b = d1.values()
+print(b)
+
+for x in b:
+    print(d1[x])
+
+
+
