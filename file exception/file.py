fa5fc2ca3cfaef9d2a7a34e0818193984ec24f30
Abhi290703 <abhigec6@gmail.com>
2025-09-22 19:56:36 +0530

    add file

---
+++ file exception/file.py
@@ -0,0 +1,33 @@
+'''
+what is file exception ?
+
+exception:
+exception occurs in program when an error is occured due to some file missing and etc
+
+file exception :
+file exception means erros in program but it handles the program
+
+syntax :
+try :
+    code
+exception :
+    code
+else:
+    code
+finally:
+    code
+
+
+try :
+it runs the main code inside the try block 
+
+except :
+you can catch specific errors in the code by throwing if the code
+
+'''
+
+# program 
+
+
+
+
