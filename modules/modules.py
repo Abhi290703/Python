11cd61be61a18115ab9ec9d1afc79232afa0a1a9
Abhi290703 <abhigec6@gmail.com>
2025-09-22 19:57:37 +0530

    add modules

---
+++ modules/modules.py
@@ -0,0 +1,28 @@
+'''
+
+what is module ?
+in python every module is pre-defined
+we can import files to another file 
+
+
+'''
+
+
+a = 890
+b = 567
+
+def sum(a,b):
+    print("sum = ",a+b)
+
+def sub(a,b):
+    print("rem = ",a-b)
+
+sum(a,b)
+sub(a,b)
+
+print("hello welcome")
+print(a)
+print(b)
+
+
+
