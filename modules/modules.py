27adfaac32687d2fe6bce0b4542139ea94e1a307
Abhi290703 <abhigec6@gmail.com>
2025-09-23 16:16:32 +0530

    add modules

--- modules/modules.py
+++ modules/modules.py
@@ -4,8 +4,27 @@ what is module ?
 in python every module is pre-defined
 we can import files to another file 
 
+their are 3 ways of import statements
+
+1.import math , pandas , libraries , etc
+2.import math as m , math as b , charts as ch
+we can define your own names in it
+3.we can take a value individually like
+from math import pi
 
 '''
+
+# example programs
+
+import math
+print(math.pi)
+
+
+import math as b
+print(b.pi)       # here we have given your own variable 
+
+from math import pi
+print(pi)
 
 
 a = 890
