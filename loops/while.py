ca058a0d6d6fc9f2ad1c83e315c20eecea1f79c3
Abhi290703 <abhigec6@gmail.com>
2025-09-25 11:54:05 +0530

    add while

---
+++ loops/while.py
@@ -0,0 +1,73 @@
+# i = 1    # variable declaration
+# while True :        # condition check
+#     i+=1            # i value should be + i=i+1
+#     print (i)       # prints 2
+#     if i<10:        # if 2 < 10
+#         continue        # continues if not break
+#     else:
+#         break
+
+# # no repetation
+
+# i = 0         
+# while i<10 :    
+#     i+=1
+#     print (i)
+# else:
+#     print("no repetition")
+
+# # ''' inreverse rightangle triangle '''
+# i=9                         # variable declaration
+# while i>0 :                 # condition if  i > 0
+#     i-=1                    # i=i-2            
+#     print("*"*(i+1))        
+
+# # ''' in rightangle triangle '''
+# i=0
+# while i<9 :
+#     i+=1
+#     print(' '*(9-i)+'*'*i)
+
+
+# # print("************************")
+
+
+# # print 1 to 10 numbers
+
+
+# # reverse of 1 to 10 numbers
+
+# i = 10
+# while i>= 0:
+#     print(i)
+#     i-=1
+
+# # multiplication table
+
+# num = int(input("enter a number"))
+# i = 1                       # starting
+# while i <= 12:              # i should less than 12
+#     print(num,"x",i,"=",num * i)        
+#     print(i)
+#     i+=1
+   
+
+# # rigth angle triangle 
+
+# a = 0
+# while a<7:
+#     print("*"*a)
+#     a+=1
+
+# #invert right angle triangle
+
+# b = 7
+# while b>0:
+#     print("*"*b)
+#     b-=1
+
+# temperature converting from celsius to kelvin
+
+temperature = 25.5
+print("Celsius -", temperature)
+print("Kelvin -", temperature + 273)
