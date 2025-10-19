fdfd558e3fddd9e4c953e47fbd5fd2ea2d970257
Abhi290703 <abhigec6@gmail.com>
2025-10-19 22:15:32 +0530

    add

---
+++ formatting tags/formatting_tags.py
@@ -0,0 +1,23 @@
+# to find the order of the alphabets 
+
+value = "A"
+print(ord(value))
+
+# c style printing
+# it is a 32gb hard drive coast about 21.90
+    # this above sentence belongs to down
+memory = 32
+device = "hardware"
+price = 21.90
+# print('it is a',memory,'gb',device,'cost about',price)
+# print('it is a %dgb %s cost about $%f'%(memory,device,price))
+# print('it is a',device, memory, 'gb cost about', price )
+# print('it is a %d gb %s cost about $%f'%(device,price,memory))
+
+
+# space conviction
+print('it is a {}gb {} cost about ${}'. format(memory,device,price))
+print('it is a {}gb {} cost about ${}'. format(device,memory,price))
+print('it is a {1}gb {0} cost about ${2}'. format(device,memory,price))
+
+print(f'it is a {memory}gb {device} cost about ${price}')
